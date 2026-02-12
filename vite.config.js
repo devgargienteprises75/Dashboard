d935eeed07dd6f107bee43fa2354688b5a241efb
devgargienteprises75 <development.gargienterprises@gmail.com>
2026-02-12 18:11:24 +0530

    Feat: Fix build bug.

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'charts': ['recharts'],
+          'icons': ['lucide-react']
+        }
+      }
+    }
+  }
 })
